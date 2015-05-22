dda7070f255c7a99594dc8dec113ff5af05c98e1
Kamal Latif <razakam79@gmail.com>
2015-05-22 15:03:41 +0100

    Modifiy initialize in model and add new db table class files

--- includes/initialize.php
+++ includes/initialize.php
@@ -3,5 +3,19 @@
 require_once("connection.php");
 require_once("functions.php");
 
+//set up database classes in model
+require_once("model/database_object.php");
+require_once("model/database.php");
+
+require_once("model/portal.php");
+require_once("model/topic.php");
+require_once("model/subtopic1.php");
+require_once("model/subtopic2.php");
+require_once("model/subtopic3.php");
+require_once("model/subtopic4.php");
+require_once("model/category.php");
+require_once("model/link.php");
+require_once("model/blogpost.php");
+
 
 ?>
